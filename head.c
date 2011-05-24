ebfacb382f6f6bc21e54edac6517937c5f2f4a5f
bmulkey <syriacman@gmail.com>
2011-05-24 10:23:46 -0700

    Edited head.c via GitHub

--- head.c
+++ head.c
@@ -1,3 +1,21 @@
+/*
+* head -- a functional clone of UNIX 'head' utility.
+* Copyright (C) 2011 Bryan Mulkey (syriacman AT gmail DOT com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -21,11 +39,9 @@ fnode_alloc()
 fnode *
 insert_after (fnode *cur, char *fname)
 {
-	puts("got here");
 	fnode *newnode = fnode_alloc ();
 	newnode->name = fname;
 	cur->next = newnode;
-	puts("returning new node");
 	return newnode;
 }
 
